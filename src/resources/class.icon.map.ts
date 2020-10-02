01d283b5370f1eaa1aafcd1e71f7ab27e08ccc9f
Srivatsa S <srivatsa.scs@gmail.com>
2020-10-03 00:33:25 +0530

    fixed some broken image  links

--- src/resources/class.icon.map.ts
+++ src/resources/class.icon.map.ts
@@ -2,11 +2,11 @@ let classMap = new Map();
 // Warrior
 classMap.set('00', { color: '#F8C300', url: 'https://wiki.guildwars2.com/images/d/db/Warrior_tango_icon_200px.png' });
 classMap.set('01', { color: '#F8C300', url: 'https://wiki.guildwars2.com/images/8/80/Berserker_tango_icon_200px.png' });
-classMap.set('02', { color: '#F8C300', url: 'https://wiki.guildwars2.com/images/6/68/Spellbreaker_tango_icon_200px.png' });
+classMap.set('02', { color: '#F8C300', url: 'https://wiki.guildwars2.com/images/7/78/Spellbreaker_tango_icon_200px.png' });
 
 classMap.set('10', { color: '#0099E1', url: 'https://wiki.guildwars2.com/images/6/6c/Guardian_tango_icon_200px.png' });
 classMap.set('11', { color: '#0099E1', url: 'https://wiki.guildwars2.com/images/1/1f/Dragonhunter_tango_icon_200px.png' });
-classMap.set('12', { color: '#0099E1', url: 'https://wiki.guildwars2.com/images/6/63/Firebrand_tango_icon_200px.png' });
+classMap.set('12', { color: '#0099E1', url: 'https://wiki.guildwars2.com/images/7/73/Firebrand_tango_icon_200px.png' });
 
 classMap.set('20', { color: '#A62019', url: 'https://wiki.guildwars2.com/images/a/a8/Revenant_tango_icon_200px.png' });
 classMap.set('21', { color: '#A62019', url: 'https://wiki.guildwars2.com/images/c/c7/Herald_tango_icon_200px.png' });
@@ -16,9 +16,9 @@ classMap.set('30', { color: '#969C9F', url: 'https://wiki.guildwars2.com/images/
 classMap.set('31', { color: '#969C9F', url: 'https://wiki.guildwars2.com/images/c/ca/Daredevil_tango_icon_200px.png' });
 classMap.set('32', { color: '#969C9F', url: 'https://wiki.guildwars2.com/images/b/b0/Deadeye_tango_icon_200px.png' });
 
-classMap.set('40', { color: '#008E44', url: 'https://wiki.guildwars2.com/images/4/41/Ranger_tango_icon_200px.png' });
-classMap.set('41', { color: '#008E44', url: 'https://wiki.guildwars2.com/images/5/5d/Druid_tango_icon_200px.png' });
-classMap.set('42', { color: '#008E44', url: 'https://wiki.guildwars2.com/images/f/f5/Soulbeast_tango_icon_200px.png' });
+classMap.set('40', { color: '#008E44', url: 'https://wiki.guildwars2.com/images/5/51/Ranger_tango_icon_200px.png' });
+classMap.set('41', { color: '#008E44', url: 'https://wiki.guildwars2.com/images/6/6d/Druid_tango_icon_200px.png' });
+classMap.set('42', { color: '#008E44', url: 'https://wiki.guildwars2.com/images/f/f6/Soulbeast_tango_icon_200px.png' });
 
 classMap.set('50', { color: '#F93A2F', url: 'https://wiki.guildwars2.com/images/2/2f/Engineer_tango_icon_200px.png' });
 classMap.set('51', { color: '#F93A2F', url: 'https://wiki.guildwars2.com/images/3/3a/Scrapper_tango_icon_200px.png' });
@@ -28,12 +28,12 @@ classMap.set('60', { color: '#C93718', url: 'https://wiki.guildwars2.com/images/
 classMap.set('61', { color: '#C93718', url: 'https://wiki.guildwars2.com/images/9/90/Tempest_tango_icon_200px.png' });
 classMap.set('62', { color: '#C93718', url: 'https://wiki.guildwars2.com/images/3/31/Weaver_tango_icon_200px.png' });
 
-classMap.set('70', { color: '#A652BB', url: 'https://wiki.guildwars2.com/images/6/63/Mesmer_tango_icon_200px.png' });
+classMap.set('70', { color: '#A652BB', url: 'https://wiki.guildwars2.com/images/7/73/Mesmer_tango_icon_200px.png' });
 classMap.set('71', { color: '#A652BB', url: 'https://wiki.guildwars2.com/images/8/8b/Chronomancer_tango_icon_200px.png' });
 classMap.set('72', { color: '#A652BB', url: 'https://wiki.guildwars2.com/images/a/a9/Mirage_tango_icon_200px.png' });
 
 classMap.set('80', { color: '#008369', url: 'https://wiki.guildwars2.com/images/c/cd/Necromancer_tango_icon_200px.png' });
-classMap.set('81', { color: '#008369', url: 'https://wiki.guildwars2.com/images/9/94/Reaper_tango_icon_200px.png' });
+classMap.set('81', { color: '#008369', url: 'https://wiki.guildwars2.com/images/9/95/Reaper_tango_icon_200px.png' });
 classMap.set('82', { color: '#008369', url: 'https://wiki.guildwars2.com/images/8/8a/Scourge_tango_icon_200px.png' });
 
 classMap.set('90', { color: '#FFFFFF', url: 'https://wiki.guildwars2.com/images/e/eb/Any_tango_icon_200px.png' });
