478c7be08bc7ea1e03a090d207be974541d31266
Srivatsa S <srivatsa.scs@gmail.com>
2020-10-05 06:51:40 +0530

    started working on !raid

--- src/gw2api/gw2api.main.ts
+++ src/gw2api/gw2api.main.ts
@@ -1,6 +1,5 @@
 import { validateGw2ApiToken } from './check.apikey.perms';
 import { checkTradingPost } from './check.trading.post';
-import * as config from '../../config/config.json';
 
 export async function maingw2(apiKey: string) {
 	if (await validateGw2ApiToken(apiKey)) {
