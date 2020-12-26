441c27458c1b0e441e8c2052f3eccc5315ebe84e
Amandeep Sagoo <amandeepksagoo+git@gmail.com>
2020-12-26 00:41:22 +0000

    amended start bot script

--- build-bot.sh
+++ build-bot.sh
@@ -7,13 +7,13 @@ pwd
 echo "Environment set to ${NODE_ENV}"
 
 if [ "${NODE_ENV}" = "production" ] ; then 
-    npm install --production=false;
-    npm list;
-    npm run build;
-    rm -r ./src;
-    mv ./dist/src ./src;
-    rm -r ./dist;
-    npm prune --production ;
+    npm install --production=false \
+    && npm run build ;
+    #\
+    #&& rm -r ./src \
+    #&& mv ./dist/src ./src \
+    #&& rm -r ./dist \
+    #&& npm prune --production ;
 elif [ "${NODE_ENV}" = "development" ] ; then
     npm install --production=false ;
 else
