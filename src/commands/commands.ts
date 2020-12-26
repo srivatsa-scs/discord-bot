9171dd86306f098b42ea6008b9b4850235e00a25
Srivatsa S <srivatsa.scs@gmail.com>
2020-12-26 18:14:03 +0530

    removed useless newlines

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -1,5 +1,4 @@
-import { Client, Message } from 'discord.js';
-import fs from 'fs';
+import { Message } from 'discord.js';
 
 export default {
 	name: 'commands',
@@ -9,13 +8,10 @@ export default {
 	usage: [' '],
 	tooltip: ['Displays a list of commands.'],
 	execute(client: any, message: Message, args: Array<string>) {
-		const commandFiles = fs.readdirSync('./src/commands').filter((file) => file.endsWith('.ts'));
 		let commands: string = '';
-
 		client.commands.forEach((value: any, key: any) => {
 			commands += `!${key} `;
 		});
-
 		message.channel.send(`<@!${message.author.id}> here are the list of commands: ${commands}`);
 	},
 };
